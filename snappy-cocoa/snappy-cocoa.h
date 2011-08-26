58ab023c5622125bac469b44f97352e269828fa8
Daniel Schnell <daniel.schnell@gmx.de>
2011-08-26 17:04:14 +0000

    Initial Commit

---
+++ snappy-cocoa/snappy-cocoa.h
@@ -0,0 +1,13 @@
+//
+//  snappy-cocoa.h
+//  snappy-cocoa
+//
+//  Created by dschnell on 26.08.11.
+//  Copyright 2011 Daniel Schnell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface snappy_cocoa : NSObject
+
+@end
---
+++ snappy-cocoa/snappy-cocoa.m
@@ -0,0 +1,23 @@
+//
+//  snappy-cocoa.m
+//  snappy-cocoa
+//
+//  Created by dschnell on 26.08.11.
+//  Copyright 2011 Daniel Schnell. All rights reserved.
+//
+
+#import "snappy-cocoa.h"
+
+@implementation snappy_cocoa
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
